dd112d60999fe205cdbe0776c26ba062484c94a2
Matias <matilarri@yahoo.com.ar>
2020-05-24 21:12:51 -0300

    agregue el main
    puto el que lee

--- TP/main.py
+++ TP/main.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 12:36:53 2020
+
+@author: 
+"""
+
+
 import random as rd
 import numpy as np
 
@@ -13,7 +21,7 @@ lim_gamma = [1, 10]
 lim_alfa = [1, 10]
 lim_Nmax = [1, 10]              #Hay que revisar estos limites porque el filtro DEWMA ya hace una estimacion de N usando estos valores
 lim_Nmin = [1, 10]              #Quiza estos parametros hay que incluirlos en los limites de arriba, para pensar
-
+nGen=1;
 
 def param_rand():
     #Genera los parametros aleatorios y los devuelve en una lista
@@ -40,13 +48,15 @@ def create_pop(num_ind):
 
 
 def read_pop():
+    print('read_pop')
     #Funcion que lee el archivo de la poblacion almacenada en el archivo
-
+   
     pass
 
 
 
 def run_test(param):
+    print('run_test')
     #Funcion que corre los 5 parametros recividos como lista en el filtro DEWMA
     #deve devolver la curva resultado del filtro
     #este filtro debe recivir el vector de valores de contagio del COVID
@@ -59,6 +69,7 @@ def run_test(param):
 
 
 def eval_test(curva_filtrada):
+    print('eval_test')
     #Recive la curva filtrada y toma de la curva del contagio
     #comparando las 2 y haciendo la evaluacion (error cuadratico medio o error medio)
     #devuelve un valor como resultado de esa comparacion
@@ -69,6 +80,7 @@ def eval_test(curva_filtrada):
 
 
 def score_ind():
+    print('score_ind')
     #Esta funcion deberia tomar el error de la funcion eval_test y asignar un puntaje 
     #quiza esta funcion este de mas.... probablemente.... casi seguro....
 
@@ -76,6 +88,7 @@ def score_ind():
 
 
 def select_ind():
+    print('select_ind')
     #Funcion que recorre la poblacion viendo los puntajes y los pasa a la proxima generacion
     #hay que definir la cantidad de individuos por "promocion directa" y cuantos por "apareamiento"
 
@@ -83,6 +96,7 @@ def select_ind():
 
 
 def mate_ind():
+    print('mate_ind')
     #Esta funcion deberia reccorer la poblacion actual e ir seleccionando los individuos a aparear
     #para crear uno nuevo
     #Debe considerar cuantos "puestos libres" hay en la proxima poblacion para no exceder el numero
@@ -95,4 +109,21 @@ def mutac_ind():
 
     pass
 
+print('Vamos a tomar',nGen,'generaciones')
+#tomar poblacion
+create_pop(10);
+#read_pop();
+for fin in range(nGen):
+    #aplicar  filtro a los tipitos
+    run_test(nGen);
+    #Evaluacion de la salida del filtro
+    eval_test(nGen);
+    #Seleccion de individuos
+    select_ind();
+    #cruza
+    mate_ind();
+    #mutacion
+    mutac_ind();
+
+#Termino y muestro resultados
 
